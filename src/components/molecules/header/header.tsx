cd001a34e84bc7b1a11ac5a02db18903cae3136d
Your NameDevbruno08 <bruno_emonteiro@hotmail.com>
2023-01-25 22:34:16 -0300

    feat: create component header

---
+++ src/components/molecules/header/header.tsx
@@ -0,0 +1,66 @@
+import { Link, useNavigate } from "react-router-dom";
+import Modal from "react-modal";
+import { MdOutlineNoteAdd } from "react-icons/md";
+import { useState } from "react";
+import { HeaderComponent, HeaderLogo, HeaderSection, Buttons } from "./styles";
+import { Form } from "../form/form";
+
+const customStyles = {
+  content: {
+    top: "35%",
+    left: "70%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-80%",
+    width: "25rem",
+    height: "20rem",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "#000",
+    borderRadius: "15px",
+    border: "none",
+  },
+  overlay: {
+    background: "rgba(0, 0, 0, 0.6)",
+  },
+};
+
+Modal.setAppElement("#root");
+
+interface HeaderProps {
+  controlPage: () => void;
+}
+
+export function Header({ controlPage }: HeaderProps) {
+  const [modalIsOpen, setModalOpen] = useState(false);
+
+  function handleModal() {
+    setModalOpen(!modalIsOpen);
+  }
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <HeaderComponent>
+        <HeaderLogo>
+          <Link to="/">
+            <h1>DevFlix</h1>
+          </Link>
+        </HeaderLogo>
+        <HeaderSection>
+          <Buttons color="purple" onClick={handleModal}>
+            <MdOutlineNoteAdd size={24} />
+          </Buttons>
+        </HeaderSection>
+      </HeaderComponent>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={handleModal}
+        style={customStyles}
+        contentLabel="Create Form"
+      >
+        <Form update={controlPage} handleModal={handleModal} />
+      </Modal>
+    </>
+  );
+}
