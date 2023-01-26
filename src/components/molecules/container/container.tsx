8d26e09797edc2292104c5902896c7a0a9f09f22
Your NameDevbruno08 <bruno_emonteiro@hotmail.com>
2023-01-25 22:17:27 -0300

    feat: implmenting container function

--- src/components/molecules/container/container.tsx
+++ src/components/molecules/container/container.tsx
@@ -1,10 +1,52 @@
 import { useNavigate } from "react-router-dom";
-import { api } from "../../utils/api/api";
-import { Product } from "../../utils/types/products.type";
-import { CardContent, CardSection, ButtonSection } from "./styles";
+import { api } from "../../../utils/api/api";
+import { Channel } from "../../../utils/types/channel.type";
+import { ContainerContent, ContainerSection, ButtonSection } from "./styles";
 
-export default function container() {
+interface ContainerProps extends Channel {
+  update: () => void;
+}
+
+export function Container({
+  id,
+  name,
+  description,
+  imageURL,
+  update,
+}: ContainerProps) {
+  const navigate = useNavigate();
+
+  async function DeleteContainer() {
+    const toDelete = await api.deleteChannelById(id);
+    if (toDelete) {
+      update();
+    }
+  }
+
   return (
-    <div>container</div>
-  )
+    <ContainerSection>
+      <ContainerContent>
+        <img src={imageURL} alt={name} />
+        <h2>{name}</h2>
+        <p>{description}</p>
+      </ContainerContent>
+      <ButtonSection>
+        <button
+          onClick={() => {
+            navigate("/update/" + id);
+          }}
+        >
+          Update
+        </button>
+        <button onClick={DeleteContainer}>Delete</button>
+        <button
+          onClick={() => {
+            navigate("/channel/" + id);
+          }}
+        >
+          See +
+        </button>
+      </ButtonSection>
+    </ContainerSection>
+  );
 }
